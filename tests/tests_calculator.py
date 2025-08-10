8e993522f6bbc705c8f961f369b667da109006fa
htmtmx <cesar.hpp96@hotmail.com>
2025-08-10 12:40:39 -0600

    Refactor calculator tests to use assertEqual for consistency and clarity

--- tests/tests_calculator.py
+++ tests/tests_calculator.py
@@ -5,13 +5,13 @@ from src.calculator import addition, division, multiplication, substraction
 
 class CalculatorTest(unittest.TestCase):
     def test_sum(self):
-        assert addition(2, 3) == 5
+        self.assertEqual(addition(2, 3), 5, "Are not equal")
 
     def test_substract(self):
-        assert substraction(5, 3) == 2
+        self.assertEqual(substraction(5, 3), 2, "Are not equal")
 
     def test_multiplication(self):
-        assert multiplication(3, 5) == 15
+        self.assertEqual(multiplication(3, 5), 15, "Are not equal")
 
     def test_division_by_zero(self):
         exception_raised = False
@@ -19,7 +19,11 @@ class CalculatorTest(unittest.TestCase):
             division(3, 0)
         except ZeroDivisionError:
             exception_raised = True
-        assert exception_raised
+        self.assertTrue(exception_raised)
 
     def test_division(self):
-        assert division(20, 5) == 4
+        self.assertEqual(division(20, 5), 4, "Are not equal")
+
+
+if __name__ == "__main__":
+    unittest.main()
