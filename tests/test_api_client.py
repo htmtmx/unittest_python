3c69605fc93d86bf4362e7f4e7a7dfa516e952aa
htmtmx <cesar.hpp96@hotmail.com>
2025-08-19 15:54:43 -0600

    Add tests for handling side effects and IP validation in get_location function

--- tests/test_api_client.py
+++ tests/test_api_client.py
@@ -1,6 +1,8 @@
 import unittest
 from unittest.mock import patch
 
+import requests
+
 from src.api_client import get_location
 
 
@@ -37,3 +39,54 @@ class ApiClientTests(unittest.TestCase):
         result = get_location(ip="8.8.8.8", country_code=country_code)
         self.assertEqual(result.get("country_code"), country_code)
         mock_get.assert_called_once_with("https://freeipapi.com/api/json/8.8.8.8")
+
+    @patch("src.api_client.requests.get")
+    def test_get_location_returns_side_effect(self, mock_get):
+        mock_get.side_effect = [
+            requests.exceptions.RequestException("Service Unavailable"),
+            unittest.mock.Mock(
+                statusCode=200,
+                json=unittest.mock.Mock(
+                    return_value={
+                        "countryName": "United States",
+                        "regionName": "California",
+                        "capital": "Washington D.C.",
+                        "cityName": "Mountain View",
+                        "countryCode": "US",
+                    }
+                ),
+            ),
+        ]
+
+        with self.assertRaises(requests.exceptions.RequestException):
+            get_location(ip="8.8.8.8", country_code="US")
+
+        result = get_location(ip="8.8.8.8", country_code="US")
+        self.assertEqual(result.get("country"), "United States")
+        self.assertEqual(result.get("region"), "California")
+        self.assertEqual(result.get("capital"), "Washington D.C.")
+        self.assertEqual(result.get("city"), "Mountain View")
+
+    @patch("src.api_client.requests.get")
+    def test_get_location_returns_validate_ip(self, mock_get):
+        mock_get.side_effect = [
+            requests.exceptions.RequestException("Value error"),
+            unittest.mock.Mock(
+                statusCode=200,
+                json=unittest.mock.Mock(
+                    return_value={
+                        "countryName": "United States",
+                        "regionName": "California",
+                        "capital": "Washington D.C.",
+                        "cityName": "Mountain View",
+                        "countryCode": "US",
+                    }
+                ),
+            ),
+        ]
+
+        with self.assertRaises(requests.exceptions.RequestException):
+            get_location(ip="300.300.300.300")
+
+        result = get_location("8.8.8.8", country_code="US")
+        self.assertEqual(result.get("country"), "United States")
